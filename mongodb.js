59006c8b6554977ce991e82658b4d71164e8c101
wasif28 <wasif28@hotmail.com>
2021-06-23 02:38:51 +0500

    More work done on mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -14,4 +14,49 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
 
     const db = client.db(dataBaseName)
 
+    db.collection('users').findOne({
+        // name: 'Wasif'
+        // _id: 60d23581818a8f0d3fa59e50                        //this wont work
+        _id: ObjectID('60d23581818a8f0d3fa59e50')               //this works
+        
+    }, (error,result)=> {
+        if (error) {
+            return console.log('Unable to Fetch User')
+        }
+        console.log(result)
+    })
+
+
+    db.collection('users').find({ name: 'Wasif' }).toArray((error, result) => {
+        if (error) {
+            return console.log('Unable to Fetch User')
+        }
+        
+        console.log(result)
+    })
+
+    db.collection('users').find({ name: 'Wasif' }).count((error, result) => {
+        if (error) {
+            return console.log('Unable to Fetch User')
+        }
+        
+        console.log(result)
+    })
+
+    db.collection('tasks').findOne({_id: ObjectID('60d23877d429f20e18e7b0ec')},(error,result)=>{
+        if (error) {
+            return console.log('Unable to Fetch Task')
+        }
+
+        console.log(result)
+    })
+
+    db.collection('tasks').find({status: false}).toArray((error,result)=>{
+        if (error) {
+            return console.log('Unable to Fetch Task/Tasks')
+        }
+
+        console.log(result)
+    })
+
 })
